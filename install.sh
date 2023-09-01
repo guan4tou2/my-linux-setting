48c5980b19e4cc792fa680affa4b4dc0af55100f
guantou <mole30608@gmail.com>
2023-09-02 01:20:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # package
-echo  "\033[36m##########install apt##########\033[m"
+echo  "\033[36m##########\ninstall apt\n##########\n\033[m"
 sudo apt update
 sudo apt install -y  lnav zsh fail2ban ca-certificates curl gnupg nodejs npm python-is-python3 unzip cargo gem fd-find ripgrep  net-tools iftop
 
 
 # oh-my-zsh
-echo  "\033[36m##########install oh-my-zsh##########\033[m"
+echo  "\033[36m##########\ninstall oh-my-zsh\n##########\n\033[m"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo apt update
@@ -22,12 +22,12 @@ sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlight
 source ~/.zshrc
 
 # fonts
-echo  "\033[36m##########install fonts##########\033[m"
+echo  "\033[36m##########\ninstall fonts\n##########\n\033[m"
 sudo add-apt-repository universe
 sudo apt install -y fonts-firacode
 
 # thefuck
-echo  "\033[36m##########install thefuck##########\033[m"
+echo  "\033[36m##########\ninstall thefuck\n##########\n\033[m"
 sudo apt update
 sudo apt install -y python3-dev python3-pip python3-setuptools
 sudo -H pip3 install thefuck
@@ -36,7 +36,7 @@ echo 'eval $(thefuck --alias)' >> ~/.zshrc
 source ~/.zshrc
 
 # nvim
-echo  "\033[36m##########install nvim##########\033[m"
+echo  "\033[36m##########\ninstall nvim\n##########\n\033[m"
 sudo snap install nvim --classic --channel=latest/edge
 sudo apt install -y python3-neovim python3-venv
 git clone https://github.com/LazyVim/starter ~/.config/nvim
@@ -44,14 +44,14 @@ rm -rf ~/.config/nvim/.git
 npm install -g neovim
 
 # lazygit
-echo  "\033[36m##########install lazygit##########\033[m"
+echo  "\033[36m##########\ninstall lazygit\n##########\n\033[m"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
 # docker
-echo  "\033[36m##########install docker##########\033[m"
+echo  "\033[36m##########\ninstall docker\n##########\n\033[m"
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -66,6 +66,6 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 # lazydocker
-echo  "\033[36m##########install lazydocker##########\033[m"
+echo  "\033[36m##########\ninstall lazydocker\n##########\n\033[m"
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 sudo mv .local/bin/lazydocker /usr/bin/ 
