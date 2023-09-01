c070ce43c9669bc226e69c025d546cd8bb12b446
guantou <mole30608@gmail.com>
2023-09-02 01:16:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,13 @@
 #!/bin/sh
 
 # package
-echo "apt"
+echo  "\033[36m##########install apt##########\033[m"
 sudo apt update
 sudo apt install -y  lnav zsh fail2ban ca-certificates curl gnupg nodejs npm python-is-python3 unzip cargo gem fd-find ripgrep  net-tools iftop
 
-# docker
-echo -e "\033[36mdocker\033[m"
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd -f docker
-sudo usermod -aG docker $USER
-newgrp docker
 
-echo -e "\033[36moh-my-zsh\033[m"
 # oh-my-zsh
+echo  "\033[36m##########install oh-my-zsh##########\033[m"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sudo apt update
@@ -36,12 +22,12 @@ sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlight
 source ~/.zshrc
 
 # fonts
-echo -e "\033[36mfonts\033[m"
+echo  "\033[36m##########install fonts##########\033[m"
 sudo add-apt-repository universe
 sudo apt install -y fonts-firacode
 
 # thefuck
-echo -e "\033[36mthefuck\033[m"
+echo  "\033[36m##########install thefuck##########\033[m"
 sudo apt update
 sudo apt install -y python3-dev python3-pip python3-setuptools
 sudo -H pip3 install thefuck
@@ -50,7 +36,7 @@ echo 'eval $(thefuck --alias)' >> ~/.zshrc
 source ~/.zshrc
 
 # nvim
-echo -e "\033[36mnvim\033[m"
+echo  "\033[36m##########install nvim##########\033[m"
 sudo snap install nvim --classic --channel=latest/edge
 sudo apt install -y python3-neovim python3-venv
 git clone https://github.com/LazyVim/starter ~/.config/nvim
@@ -58,11 +44,28 @@ rm -rf ~/.config/nvim/.git
 npm install -g neovim
 
 # lazygit
+echo  "\033[36m##########install lazygit##########\033[m"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
+# docker
+echo  "\033[36m##########install docker##########\033[m"
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd -f docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 # lazydocker
+echo  "\033[36m##########install lazydocker##########\033[m"
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 sudo mv .local/bin/lazydocker /usr/bin/ 
