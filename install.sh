ab581a8d0f1b19cdb919807ec09b71bb3bf0322a
guantou <mole30608@gmail.com>
2023-09-17 04:07:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,8 @@ timedatectl
 echo  "\033[36m##########\ninstall apt\n##########\n\033[m"
 sudo apt update
 sudo apt install -y  lnav zsh fail2ban ca-certificates curl gnupg nodejs npm python-is-python3 unzip cargo gem fd-find ripgrep  net-tools iftop tldr fzf ncdu logwatch \
-                      lua5.4
+                      lua5.4 stress
+sudo pip install s-tui
 
 # fail2bam
 echo  "\033[36m##########\nsetting fail2ban\n##########\n\033[m"
