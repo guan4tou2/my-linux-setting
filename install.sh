42235b010f667b203107387ec02e96ed1074bd4f
guantou <mole30608@gmail.com>
2024-08-18 00:14:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ echo  "\033[36m##########\ninstall apt\n##########\n\033[m"
 sudo apt update
 sudo apt install -y  lnav zsh fail2ban ca-certificates curl gnupg nodejs npm python-is-python3 unzip cargo gem fd-find ripgrep  net-tools iftop tldr fzf ncdu logwatch \
                       lua5.4 stress
-sudo pip install s-tui
+sudo pip install s-tui --break-system-packages
 
 # fail2bam
 echo  "\033[36m##########\nsetting fail2ban\n##########\n\033[m"
@@ -34,6 +34,10 @@ sudo git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 sed -i -e 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-z)/g' ~/.zshrc
 
+# p10k
+echo  "\033[36m##########\nsetting p10k\n##########\n\033[m"
+wget https://raw.githubusercontent.com/guan4tou2/my-linux-setting/main/.p10k.zsh -O ~/.p10k.zsh
+
 # fonts
 echo  "\033[36m##########\ninstall fonts\n##########\n\033[m"
 sudo add-apt-repository universe
@@ -43,7 +47,7 @@ sudo apt install -y fonts-firacode
 echo  "\033[36m##########\ninstall thefuck\n##########\n\033[m"
 sudo apt update
 sudo apt install -y python3-dev python3-pip python3-setuptools
-sudo -H pip3 install thefuck
+sudo -H pip3 install thefuck --break-system-packages
 echo 'export PATH="$PATH:~/.local/bin"' >> ~/.zshrc
 echo 'eval $(thefuck --alias)' >> ~/.zshrc
 
@@ -54,6 +58,7 @@ sudo apt install -y python3-neovim python3-venv
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 sudo npm install -g neovim
+echo 'alias nv="nvim"' >> ~/.zshrc
 
 # lazygit
 echo  "\033[36m##########\ninstall lazygit\n##########\n\033[m"
@@ -61,6 +66,8 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm -rf lazygit
+rm lazygit.tar.gz
 
 # docker
 echo  "\033[36m##########\ninstall docker\n##########\n\033[m"
@@ -76,17 +83,8 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 sudo groupadd -f docker
 sudo usermod -aG docker $USER
 
-# lazydocker arch
+# lazydocker
 echo  "\033[36m##########\ninstall lazydocker\n##########\n\033[m"
-if [ "$ARCH" = "match" ]
-then
-  curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-  sudo mv .local/bin/lazydocker /usr/bin/ 
-else
-  echo "skip install lazydocker, $ARCH not match"
-fi
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
-# p10k
-echo  "\033[36m##########\nsetting p10k\n##########\n\033[m"
-wget https://raw.githubusercontent.com/guan4tou2/my-linux-setting/main/.p10k.zsh
 source ~/.zshrc
