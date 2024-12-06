37b94f0df838b9818f679aec14626901168825c5
guantou <mole30608@gmail.com>
2024-12-06 23:59:15 +0800

    Update install.sh
    improve docker install method

--- install.sh
+++ install.sh
@@ -77,17 +77,10 @@ rm lazygit.tar.gz
 
 # docker
 echo  "\033[36m##########\ninstall docker\n##########\n\033[m"
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
-sudo groupadd -f docker
-sudo usermod -aG docker $USER
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+# sudo groupadd -f docker
+# sudo usermod -aG docker $USER
 
 # lazydocker
 echo  "\033[36m##########\ninstall lazydocker\n##########\n\033[m"
